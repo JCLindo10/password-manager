1afb06d580297bf8aeb0cac4c6de1a6cd3a97217
Jakob Lindo <JCLindo10@gmail.com>
2023-11-17 12:44:03 -0500

    more descriptive test names, more test cases

--- tests/test_pwgen.py
+++ tests/test_pwgen.py
@@ -23,11 +23,11 @@ class Test_Password_Generation(unittest.TestCase):
             self.num_and_special_passwords[num] = pwgen.gen(self.test_depth, special_chars=True, numbers=True)
 
 
-    def test_length(self):
+    def test_password_length(self):
         for idx, password in enumerate(self.len_passwords):
             self.assertEqual(idx, len(password))
 
-    def test_special_chars(self):
+    def test_passwords_with_special_chars_have_special_chars(self):
         import string
         num_specials_each_password = [0] * self.test_depth
         for idx, password in enumerate(self.special_passwords):
@@ -38,7 +38,7 @@ class Test_Password_Generation(unittest.TestCase):
         for num in num_specials_each_password:
             self.assertTrue(num > 0)
 
-    def test_numbers(self):
+    def test_passwords_with_numbers_contain_numbers(self):
         import string
         num_digits_each_password = [0] * self.test_depth
         for idx, password in enumerate(self.num_passwords):
@@ -49,7 +49,7 @@ class Test_Password_Generation(unittest.TestCase):
         for num in num_digits_each_password:
             self.assertTrue(num > 0)
 
-    def test_numbers_and_special_chars(self):
+    def test_passwords_with_numbers_and_special_chars_havenumbers_and_special_chars(self):
         import string
         num_digits_or_specials_each_password = [0] * self.test_depth
         for idx, password in enumerate(self.num_and_special_passwords):
@@ -60,7 +60,17 @@ class Test_Password_Generation(unittest.TestCase):
         for num in num_digits_or_specials_each_password:
             self.assertTrue(num > 0)
 
+    def test_passwords_containing_numbers_have_correct_length(self):
+        for password in self.num_passwords:
+            self.assertEqual(self.test_depth, len(password))
 
+    def test_passwords_containing_special_chars_have_correct_length(self):
+        for password in self.special_passwords:
+            self.assertEqual(self.test_depth, len(password))
+
+    def test_passwords_containing_numbers_and_chars_have_correct_length(self):
+        for password in self.num_and_special_passwords:
+            self.assertEqual(self.test_depth, len(password))
 
 if __name__ == "__main__":
     unittest.main()
