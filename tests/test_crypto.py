7a9baa93c715bfc1d5b75d5df9b4e42488eca8b9
Jakob Lindo <JCLindo10@gmail.com>
2023-11-21 16:23:40 -0500

    updated alphabet test

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -17,7 +17,10 @@ class Test_Password_Cryptography(unittest.TestCase):
         self.assertEqual(expected_hash, actual_hash)
 
     def test_abcdefghijklmnopqrstuvwxyz_sha256_hash(self):
-        self.assertEqual(self.abcdefghijklmnopqrstuvwxyz_sha256_hash, pwcrypto.encrypt("abcdefghijklmnopqrstuvwxyz"))
+        expected_hash = hashlib.sha256("abcdefghijklmnopqrstuvwxyz".encode()).hexdigest()
+        actual_hash = pwcrypto.encrypt("abcdefghijklmnopqrstuvwxyz")
+
+        self.assertEqual(expected_hash, actual_hash)
 
     def test_Password1Exclamation(self):
         self.assertEqual(self.Password1Exclamation_sha256_hash, pwcrypto.encrypt("Password1!"))
