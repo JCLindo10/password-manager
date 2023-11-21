2b7e6e5069c1bf6606a9bab01f9a0bf6e22b1e78
Jakob Lindo <JCLindo10@gmail.com>
2023-11-21 15:40:50 -0500

    password123 test

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -2,8 +2,12 @@ import unittest
 import pwcrypto
 
 class Test_Password_Cryptography(unittest.TestCase):
+
+    def setUp(self):
+        self.password123_sha256_hash = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f"
+
     def test_password123(self):
-        pass
+        self.assertEqual(self.password123_sha256_hash, pwcrypto.encrypt("password123"))
     
 if __name__ == "__main__":
     unittest.main() 
