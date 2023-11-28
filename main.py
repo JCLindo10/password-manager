da1496aacb60d84400a49c57875597fb0cb9c7da
Jakob Lindo <JCLindo10@gmail.com>
2023-11-28 04:47:40 -0500

    added specials and numerics options

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ Main driver program
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-n", "--n_chars", action="store", type="int", dest="password_length")
+parser.add_option("-n", "--num_chars", action="store", type="int", dest="password_length")
+parser.add_option("-s", "--special_chars", action="store_true", dest="special_chars")
+parser.add_option("-d", "--numerics", action="store_true", dest="numerical_chars")
 
 def main():
     pass
