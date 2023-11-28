f2a7b6dcf3de3e62b212482360a1c30277e5d13a
Jakob Lindo <JCLindo10@gmail.com>
2023-11-28 04:55:49 -0500

    help

--- main.py
+++ main.py
@@ -12,9 +12,19 @@ Main driver program
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-n", "--num_chars", action="store", type="int", dest="password_length")
-parser.add_option("-s", "--special_chars", action="store_true", dest="special_chars")
-parser.add_option("-d", "--numerics", action="store_true", dest="numerical_chars")
+parser.add_option("-n", "--num_chars", 
+                    action="store", 
+                    type="int", 
+                    dest="password_length",
+                    help="The length of the desired password")
+parser.add_option("-s", "--special_chars",
+                    action="store_true", 
+                    dest="special_chars",
+                    help="Boolean denoting the usage of special characters")
+parser.add_option("-d", "--numerics",
+                    action="store_true",
+                    dest="numerical_chars",
+                    help="Boolean denoting the usage of numbers")
 
 def main():
     options, args = parser.parse_args()
